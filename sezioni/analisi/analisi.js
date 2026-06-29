66c8835be041783b227f9b048a75c898f2fb483d
zeqjasaimon <zeqja.saimon@gmail.com>
2026-06-29 17:25:49 +0200

    Create analisi.js

---
+++ sezioni/analisi/analisi.js
@@ -0,0 +1,4 @@
+(function() {
+    // Questo modulo visualizza report grafici o analitici statici basati sui calcoli attivi.
+    console.log("Modulo Analisi Finanziaria e Food Cost Inizializzato.");
+})();
